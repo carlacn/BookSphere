a3bc585201be03a64a560b75cd07b41a3e449042
Carla Campo <carlacampo.cn@gmail.com>
2024-02-21 19:46:48 +0100

    [IMP] Basic SharedPreferences added (commented code - working, checked in logcat)

--- app/src/main/java/com/carlacampo/booksphere/design/MainActivity.kt
+++ app/src/main/java/com/carlacampo/booksphere/design/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.carlacampo.booksphere.bbdd.BooksDBHelper
 import com.carlacampo.booksphere.design.addbook.AddBookScreen
 import com.carlacampo.booksphere.design.addbook.AddBookScreenViewModel
 import com.carlacampo.booksphere.model.Routes
@@ -24,34 +23,23 @@ class MainActivity : ComponentActivity() {
         val screenCurrentReadViewModel: ScreenCurrentReadViewModel by viewModels()
         val screenDesiredViewModel: ScreenDesiredViewModel by viewModels()
         val addBookScreenViewModel: AddBookScreenViewModel by viewModels()
-
         //A variable for each view model you have and they are all called in this main.
 
-        /*val dbHelper = BooksDBHelper(this)
-        val writableDB = dbHelper.readableDatabase
-         writableDB.execSQL(
-            """
-                INSERT INTO ${BookDBScheme.TABLE_NAME}(d
-                    ${BookDBScheme.COLUMN_NAME},
-                    ${BookDBScheme.COLUMN_AUTHOR},
-                    ${BookDBScheme.COLUMN_PUBLICATION_YEAR},
-                    ${BookDBScheme.COLUMN_EDITORIAL},
-                    ${BookDBScheme.COLUMN_PAGES}
-                )
-                VALUES("El Silmarillion", "J. R. R. Tolkien", 1901, "Booket", 448)
-            """.trimIndent()
-        )
+
+        /*
+        SharedPreferences
+        val sharedprefs = this.getSharedPreferences("Shared1", Context.MODE_PRIVATE)
+        //sharedprefs.edit().putInt("key1", 1).apply()
 
 
-        val values = ContentValues().apply {
-            put(BookDBScheme.COLUMN_NAME, "El Hobbit")
-            put(BookDBScheme.COLUMN_AUTHOR, "J. R. R. Tolkien")
-            put(BookDBScheme.COLUMN_PUBLICATION_YEAR, 2022)
-            put(BookDBScheme.COLUMN_EDITORIAL, "Booket")
-            put(BookDBScheme.COLUMN_PAGES, 288)
+        sharedprefs.edit().apply {
+            putInt("key1", 1)
+            apply()
         }
-        writableDB.insert(BookDBScheme.TABLE_NAME, null, values)
-   */
+        val savedShare = sharedprefs.getInt("key1",9)
+        Log.d("Lifecycle", "onCreate: $savedShare")
+        */
+
         super.onCreate(savedInstanceState)
         setContent {
             BookSphereTheme {
